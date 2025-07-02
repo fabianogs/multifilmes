36e3bf39035e108cf0b6a76c6bf3bb4a7deeee1d
Fabiano G. dos Santos <fabianogs@gmail.com>
2025-07-02 14:58:56 -0300

    feat: reestrutura o cabeçalho do site com novo menu dropdown e melhorias na navegação

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,25 @@
 <header class="header">
     <div class="row a-center">
-        <div class="col md-up-3"><a href="{{ route('site.home') }}" class="header-logo"><img src="{{ asset('img/logo.png') }}" alt=""></a>
+        <div class="col md-up-3"><a href="{{ route('site.home') }}" class="header-logo"><img
+                    src="{{ asset('img/logo.png') }}" alt=""></a>
             <div class="burger"><span></span> <span></span> <span></span> <span></span></div>
         </div>
         <div class="col md-up-9">
-            <nav class="header-menu"><a href="{{ route('site.categorias_solucao', 'automotivas') }}">automotivas</a> <a href="{{ route('site.categorias_solucao', 'residenciais-corporativas') }}">residenciais e
-                    corporativas</a> <a href="{{ route('site.home') }}#unidades">unidades</a> <a href="{{ route('site.blog') }}">universo multifilmes</a></nav>
+            <nav class="header-menu">
+                <ul>
+                  <li>
+                    <a>Soluções</a>
+                    <div class="header-menu-dropdown">
+                      <a href="">Lorem ipsum dolor</a>
+                      <a href="">Lorem ipsum dolor</a>
+                      <a href="">Lorem ipsum dolor</a>
+                      <a href="">Lorem ipsum dolor</a>
+                    </div>
+                  </li>
+                  <li><a href="home#unidades">unidades</a> </li>
+                  <li><a href="{{ route('site.blog') }}">universo multifilmes</a></li>
+                </ul>
+            </nav>
         </div>
     </div><a href="https://sejafranqueado.multifilmes.com.br/" target="_blank" class="btn-franchisee"><i
             class="far fa-thumbs-up"></i> seja um franqueado!</a>
